7a65afbd16dea681b14638f3c365b17e8888aadd
SaifN97 <saifnarpali@gmail.com>
2021-03-18 21:26:14 +0530

    Initial commit and tailwind setup

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Tailwind example -Twitter</title>
+
+    <link rel="stylesheet" href="/css/main.css">
+</head>
+
+<body class="bg-grey-light font-sans">
+    <div class="bg-white">
+        <div class="container mx-auto flex items-center py-4">
+
+        </div>
+    </div>
+
+</body>
+
+</html>
